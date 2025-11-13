0fd820929e50688880cbe405066cb18908f3e9bb
Juana Belen MORILLO CATENA <juana.morillo@alten.es>
2025-11-13 12:37:03 +0100

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-import { About } from './pages/about/about';
 import { Projects } from './pages/projects/projects';
 import { Contact } from './pages/contact/contact';
+import { Tecnologias } from './pages/tecnologias/tecnologias';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'sobre-mi', component: About },
   { path: 'proyectos', component: Projects },
   { path: 'contacto', component: Contact },
+  { path: 'tecnologias', component: Tecnologias },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,13 @@ import { Component, signal } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
 import { Home } from './pages/home/home';
-import { About } from './pages/about/about';
 import { Projects } from './pages/projects/projects';
 import { Contact } from './pages/contact/contact';
 import { Tecnologias } from './pages/tecnologias/tecnologias';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Footer, Home, About, Projects, Contact, Tecnologias],
+  imports: [Navbar, Footer, Home, Projects, Contact, Tecnologias],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
